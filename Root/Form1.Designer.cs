989ad1cd3ba7ee4dc5fadff9b73210eb40d470ea
Nytsui <justin.steberl@gmx.de>
2016-08-23 18:28:16 +0200

    Add files via upload

---
+++ Root/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace Root
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox = new System.Windows.Forms.GroupBox();
+            this.fillToolStrip = new System.Windows.Forms.ToolStrip();
+            this.childnameToolStripLabel = new System.Windows.Forms.ToolStripLabel();
+            this.childnameToolTextComboBox = new System.Windows.Forms.ToolStripComboBox();
+            this.fillToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.iDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.parentIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.getIDbyIDBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.database1DataSet = new Root.Database1DataSet();
+            this.btnBeenden = new System.Windows.Forms.Button();
+            this.getIDbyIDTableAdapter = new Root.Database1DataSetTableAdapters.GetIDbyIDTableAdapter();
+            this.groupBox.SuspendLayout();
+            this.fillToolStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.getIDbyIDBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox
+            // 
+            this.groupBox.Controls.Add(this.fillToolStrip);
+            this.groupBox.Controls.Add(this.dataGridView1);
+            this.groupBox.Location = new System.Drawing.Point(12, 12);
+            this.groupBox.Name = "groupBox";
+            this.groupBox.Size = new System.Drawing.Size(268, 263);
+            this.groupBox.TabIndex = 0;
+            this.groupBox.TabStop = false;
+            this.groupBox.Text = "Wählen Sie eine Seite aus und starten Sie den Vorgang";
+            // 
+            // fillToolStrip
+            // 
+            this.fillToolStrip.Dock = System.Windows.Forms.DockStyle.None;
+            this.fillToolStrip.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.fillToolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.childnameToolStripLabel,
+            this.childnameToolTextComboBox,
+            this.fillToolStripButton});
+            this.fillToolStrip.Location = new System.Drawing.Point(25, 39);
+            this.fillToolStrip.Name = "fillToolStrip";
+            this.fillToolStrip.Size = new System.Drawing.Size(235, 25);
+            this.fillToolStrip.TabIndex = 3;
+            this.fillToolStrip.Text = "fillToolStrip";
+            // 
+            // childnameToolStripLabel
+            // 
+            this.childnameToolStripLabel.Name = "childnameToolStripLabel";
+            this.childnameToolStripLabel.Size = new System.Drawing.Size(64, 22);
+            this.childnameToolStripLabel.Text = "Datenbank";
+            // 
+            // childnameToolTextComboBox
+            // 
+            this.childnameToolTextComboBox.Items.AddRange(new object[] {
+            "Hauptseite",
+            "Hauptseite",
+            "Seite 3",
+            "Seite 4",
+            "Seite 5",
+            "Seite 6",
+            "Seite 7",
+            "Seite 8",
+            "Seite 9",
+            "Seite 10",
+            "Seite 11",
+            "Seite 12",
+            "Seite 13",
+            "Seite 14",
+            "Seite 15",
+            "Seite 16",
+            "Seite 17",
+            "Seite 18",
+            "Seite 19",
+            "Seite 20",
+            "Seite 21",
+            "Seite 22",
+            "Seite 23",
+            "Fehler 1",
+            "Fehler 2"});
+            this.childnameToolTextComboBox.Name = "childnameToolTextComboBox";
+            this.childnameToolTextComboBox.Size = new System.Drawing.Size(100, 25);
+            // 
+            // fillToolStripButton
+            // 
+            this.fillToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.fillToolStripButton.Name = "fillToolStripButton";
+            this.fillToolStripButton.Size = new System.Drawing.Size(35, 22);
+            this.fillToolStripButton.Text = "Start";
+            this.fillToolStripButton.Click += new System.EventHandler(this.fillToolStripButton_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.iDDataGridViewTextBoxColumn,
+            this.nameDataGridViewTextBoxColumn,
+            this.parentIDDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.getIDbyIDBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 80);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(268, 183);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // iDDataGridViewTextBoxColumn
+            // 
+            this.iDDataGridViewTextBoxColumn.DataPropertyName = "ID";
+            this.iDDataGridViewTextBoxColumn.HeaderText = "ID";
+            this.iDDataGridViewTextBoxColumn.Name = "iDDataGridViewTextBoxColumn";
+            this.iDDataGridViewTextBoxColumn.ReadOnly = true;
+            this.iDDataGridViewTextBoxColumn.Width = 75;
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            this.nameDataGridViewTextBoxColumn.ReadOnly = true;
+            this.nameDataGridViewTextBoxColumn.Width = 75;
+            // 
+            // parentIDDataGridViewTextBoxColumn
+            // 
+            this.parentIDDataGridViewTextBoxColumn.DataPropertyName = "ParentID";
+            this.parentIDDataGridViewTextBoxColumn.HeaderText = "ParentID";
+            this.parentIDDataGridViewTextBoxColumn.Name = "parentIDDataGridViewTextBoxColumn";
+            this.parentIDDataGridViewTextBoxColumn.ReadOnly = true;
+            this.parentIDDataGridViewTextBoxColumn.Width = 75;
+            // 
+            // getIDbyIDBindingSource
+            // 
+            this.getIDbyIDBindingSource.DataMember = "GetIDbyID";
+            this.getIDbyIDBindingSource.DataSource = this.database1DataSet;
+            // 
+            // database1DataSet
+            // 
+            this.database1DataSet.DataSetName = "Database1DataSet";
+            this.database1DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // btnBeenden
+            // 
+            this.btnBeenden.Location = new System.Drawing.Point(205, 281);
+            this.btnBeenden.Name = "btnBeenden";
+            this.btnBeenden.Size = new System.Drawing.Size(75, 23);
+            this.btnBeenden.TabIndex = 1;
+            this.btnBeenden.Text = "Beenden";
+            this.btnBeenden.UseVisualStyleBackColor = true;
+            this.btnBeenden.Click += new System.EventHandler(this.btnBeenden_Click);
+            // 
+            // getIDbyIDTableAdapter
+            // 
+            this.getIDbyIDTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 316);
+            this.Controls.Add(this.btnBeenden);
+            this.Controls.Add(this.groupBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox.ResumeLayout(false);
+            this.groupBox.PerformLayout();
+            this.fillToolStrip.ResumeLayout(false);
+            this.fillToolStrip.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.getIDbyIDBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource getIDbyIDBindingSource;
+        private Database1DataSet database1DataSet;
+        private System.Windows.Forms.Button btnBeenden;
+        private Database1DataSetTableAdapters.GetIDbyIDTableAdapter getIDbyIDTableAdapter;
+        private System.Windows.Forms.ToolStrip fillToolStrip;
+        private System.Windows.Forms.ToolStripLabel childnameToolStripLabel;
+        private System.Windows.Forms.ToolStripComboBox childnameToolTextComboBox;
+        private System.Windows.Forms.ToolStripButton fillToolStripButton;
+        private System.Windows.Forms.DataGridViewTextBoxColumn iDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn parentIDDataGridViewTextBoxColumn;
+
+    }
+}
+
---
+++ Root/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Root
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void fillToolStripButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.getIDbyIDTableAdapter.Fill(this.database1DataSet.GetIDbyID, childnameToolTextComboBox.Text);
+            }
+            catch (System.Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void btnBeenden_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Vielen Dank und bis zum nächsten Mal!");
+            Application.Exit();
+        }
+    }
+}
