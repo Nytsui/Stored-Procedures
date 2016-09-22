36e784bba030892bfb67df0f0c3ea3c80af9d28f
Nytsui <justin.steberl@gmx.de>
2016-09-22 11:42:49 +0200

    Add files via upload

---
+++ Root/ClassLibrary1/Form.Test.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using NUnit.Extensions.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Root.Test
+{
+    [TestFixture]
+    public class FormTest
+    {
+        [Test]
+        public void sampleTest()
+        {
+            Assert.Pass();
+        }
+        //public void MainForm()
+        //{
+        //    FormTester formtester = new FormTester("Form1");
+        //}
+        //[Test]
+        //public void btnStart()
+        //{
+        //    ButtonTester buttonTester = new ButtonTester("fillToolStripButton_Click", "Form1");
+        //}
+        //[Test]
+        //public void btnBeenden()
+        //{
+        //    ButtonTester buttonTester = new ButtonTester("btnBeenden_Click", "Form1");
+        //}
+
+        
+    }
+}
